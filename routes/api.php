c89d1761ed2ada61f5879337d6256b7760436a3b
Sabriel <sadewobrilianrafael@gmail.com>
2022-12-08 21:35:00 +0700

    menambahkan api utility route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/reset/{type}', function($type) {
+    Artisan::call($type.':clear');
+    Artisan::call($type.':cache');
+    return 'OK';
+});
+
+Route::get('/migrate', function() {
+    Artisan::call('migrate:fresh --seed');
+    return 'OK';
+});
+
+Route::get('/run-production', function() {
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    Artisan::call('view:cache');
+    Artisan::call('optimize:clear');
+    Artisan::call('optimize');
+    return 'OK';
 });
